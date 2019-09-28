a3f14839ebd8bfba39e0dc5a644130a45d6bdf2f
Anurag Bhagsain <abhagsain@gmail.com>
2019-09-29 00:47:25 +0530

    refactor sass files

--- src/sass/sass.sh
+++ src/sass/sass.sh
@@ -5,3 +5,8 @@ touch _function.scss _mixins.scss _variables.scss
 cd .. 
 cd base 
 touch _animations.scss _typography.scss _utilities.scss _base.scss
+# @import "./abstract/variables";
+# @import "./layout/header";
+# @import "./base/animations";
+# @import "./base/typography";
+# @import "./components/button";
