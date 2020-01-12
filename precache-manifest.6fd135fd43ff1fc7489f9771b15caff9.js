6e26c2eb5c3dc29ed5dbdbe1b5de0fdd0ddf1d42
Anurag Bhagsain <abhagsain@gmail.com>
2020-01-12 11:06:13 +0530

    Updates

---
+++ precache-manifest.6fd135fd43ff1fc7489f9771b15caff9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fd966a68032b300dacf9f139676c15",
+    "url": "/cupido/index.html"
+  },
+  {
+    "revision": "778b4946c53d70c1c69e",
+    "url": "/cupido/static/css/main.1501bd86.chunk.css"
+  },
+  {
+    "revision": "3f092f7475d26ac59986",
+    "url": "/cupido/static/js/2.5a301fa0.chunk.js"
+  },
+  {
+    "revision": "778b4946c53d70c1c69e",
+    "url": "/cupido/static/js/main.0d90ae52.chunk.js"
+  },
+  {
+    "revision": "2bdabfa1296bb3918584",
+    "url": "/cupido/static/js/runtime-main.63b6c02e.js"
+  },
+  {
+    "revision": "cc180fe2bd412af1d9487573fc318879",
+    "url": "/cupido/static/media/couple-image-1-1x.cc180fe2.jpg"
+  },
+  {
+    "revision": "8c9d4be701c3837e9b9db5940dbcc217",
+    "url": "/cupido/static/media/couple-image-1-2x.8c9d4be7.jpg"
+  },
+  {
+    "revision": "01916c14a1b428abad33a9670476fd39",
+    "url": "/cupido/static/media/couple-image-2-1x.01916c14.jpg"
+  },
+  {
+    "revision": "02323abb693776ca58f71cd9d1a39ce4",
+    "url": "/cupido/static/media/couple-image-2-2x.02323abb.jpg"
+  },
+  {
+    "revision": "00853a64bcbb5111803c9b2fc370193e",
+    "url": "/cupido/static/media/couple-image-3-1x.00853a64.jpg"
+  },
+  {
+    "revision": "87d789dccf1ce4e20e4ab45687dd1572",
+    "url": "/cupido/static/media/couple-image-3-2x.87d789dc.jpg"
+  },
+  {
+    "revision": "92d2dc9aefba638af3fb08d37f8cee78",
+    "url": "/cupido/static/media/dating-bg-1x.92d2dc9a.jpg"
+  },
+  {
+    "revision": "20a09046b6e9c29c9d6f3893769bfad1",
+    "url": "/cupido/static/media/dating-bg-2x.20a09046.jpg"
+  },
+  {
+    "revision": "435c89358347a081266f0e4f21061a42",
+    "url": "/cupido/static/media/form-bg.435c8935.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cupido/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "2db1473d29a892f74b1d2f8af92e8135",
+    "url": "/cupido/static/media/profile-1-1x.2db1473d.jpg"
+  },
+  {
+    "revision": "4bf05f920f696dd51602df66396b0dc7",
+    "url": "/cupido/static/media/profile-2-1x.4bf05f92.jpg"
+  },
+  {
+    "revision": "65111e20e7a7f775e55ad134bf648498",
+    "url": "/cupido/static/media/profile-2-2x.65111e20.jpg"
+  },
+  {
+    "revision": "e6d42c268ba764178f7a2b3c76a24baf",
+    "url": "/cupido/static/media/profile-3-1x.e6d42c26.jpg"
+  },
+  {
+    "revision": "e6d42c268ba764178f7a2b3c76a24baf",
+    "url": "/cupido/static/media/profile-3-2x.e6d42c26.jpg"
+  },
+  {
+    "revision": "d6f72a2fd2e95b74f9ebb8224792600c",
+    "url": "/cupido/static/media/profile-4-1x.d6f72a2f.jpg"
+  },
+  {
+    "revision": "ecde8444d464f4e082e8aaf8822604ec",
+    "url": "/cupido/static/media/profile-4-2x.ecde8444.jpg"
+  },
+  {
+    "revision": "58fc3bb8dd43438532822714ae1fb7b0",
+    "url": "/cupido/static/media/test-1-1x.58fc3bb8.jpg"
+  },
+  {
+    "revision": "5c811fd4832b40d95139f45e97affdcb",
+    "url": "/cupido/static/media/test-1-2x.5c811fd4.jpg"
+  },
+  {
+    "revision": "d4e3c5a5c94e3a6e884efeda8a0dde8f",
+    "url": "/cupido/static/media/test-2-1x.d4e3c5a5.jpg"
+  },
+  {
+    "revision": "ee653856bf6a47ddcb53f0ae69e5acbf",
+    "url": "/cupido/static/media/test-2-2x.ee653856.jpg"
+  },
+  {
+    "revision": "8f65ea92311a5891e0f0ceb5045bb7d6",
+    "url": "/cupido/static/media/test-bg.8f65ea92.jpg"
+  }
+]);
